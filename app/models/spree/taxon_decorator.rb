f103b0f8ca17e645e4fb861aac2b604092b756bc
GarPit <garik.piton@gmail.com>
2013-12-18 18:11:04 +0400

    show all properties for adding to taxon, not limited to 10

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -5,7 +5,7 @@ Spree::Taxon.class_eval do
   def get_properties(name_cont)
     mas=[]
     if mas.empty?
-      mas=Spree::Property.joins(products: :taxons).where("(spree_taxons.lft between #{self.lft} AND #{self.rgt} AND spree_taxons.rgt between #{self.lft} AND #{self.rgt}AND parent_id IS NOT NULL) AND presentation LIKE '%#{name_cont}%' ").limit(10).flatten.first(10)
+      mas=Spree::Property.joins(products: :taxons).where("(spree_taxons.lft between #{self.lft} AND #{self.rgt} AND spree_taxons.rgt between #{self.lft} AND #{self.rgt}AND parent_id IS NOT NULL) AND presentation LIKE '%#{name_cont}%' ").uniq
     end
     return mas
   end
--- app/overrides/add_taxon_properties_field.rb
+++ app/overrides/add_taxon_properties_field.rb
@@ -2,6 +2,6 @@ Deface::Override.new(:virtual_path => "spree/admin/taxons/_form",
                      :insert_after => 'erb[loud]:contains("f.field_container :icon")',
                      :text => '<%= f.field_container :property_ids do %>
                      <%= f.label :property_ids, Spree.t(:properties) %><br>
-                     <%= f.select :property_ids, @taxon.get_properties("").uniq.map{|e| [e.presentation, e.id]}, {}, {:class => "select2 fullwidth", :multiple => true} %>
+                     <%= f.select :property_ids, @taxon.get_properties("").map{|e| [e.presentation, e.id]}, {}, {:class => "select2 fullwidth", :multiple => true} %>
                       <% end %>',
                      :name => "add_taxon_properties_field")
