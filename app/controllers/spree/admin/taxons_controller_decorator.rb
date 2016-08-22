187ee6e9de744aa0080ac735c1becb586e6a5680
GarPit <garik.piton@gmail.com>
2016-08-22 17:37:41 +0300

    fix for supporting spree 3.1

--- app/controllers/spree/admin/taxons_controller_decorator.rb
+++ app/controllers/spree/admin/taxons_controller_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Admin::TaxonsController.class_eval do
 
-  private
-
-  def permitted_params
-    [:name, :parent_id, :position, :icon, :description, :permalink,
-     :taxonomy_id, :meta_description, :meta_keywords, :meta_title, {:property_ids => []}]
+  def permitted_taxon_attributes
+    super + [
+      property_ids: []
+    ]
   end
+
 
 end
